2d2e0e6ba38867458b27a590f7da20a4777f3624
ElNino Wang <elninowang@gmail.com>
2017-11-14 10:05:52 +0800

    完成strat和restart动画

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -3,9 +3,61 @@ using System.Collections;
 
 public class GameLogic : MonoBehaviour
 {
-	public GameObject startUI, restartUI;
+    public GameObject player;
+    public GameObject startUI, restartUI;
+    public GameObject startPoint, playPoint, restartPoint;
 
-	public void ToggleUI()
+    void Start()
+    {
+        // Update 'player' to be the camera's parent gameobject, i.e. GvrEditorEmulator, instead of the camera itself.
+        // Required because GVR resets camera position to 0, 0, 0.
+        player = player.transform.parent.gameObject;
+
+        // Move player to the start position.
+        player.transform.position = startPoint.transform.position;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && player.transform.position == playPoint.transform.position)
+        {
+            PuzzleSuccess();
+        }
+    }
+
+    // Begin the puzzle sequence.
+    public void StartPuzzle()
+    {
+        ToggleUI();
+        iTween.MoveTo(player,
+            iTween.Hash(
+                "position", playPoint.transform.position,
+                "time", 5,
+                "easetype", "linear"
+            )
+        );
+    }
+
+    // Reset the puzzle sequence.
+    public void ResetPuzzle()
+    {
+        player.transform.position = startPoint.transform.position;
+        ToggleUI();
+    }
+
+    // Do this when the player solves the puzzle.
+    public void PuzzleSuccess()
+    {
+        iTween.MoveTo(player,
+            iTween.Hash(
+                "position", restartPoint.transform.position,
+                "time", 5,
+                "easetype", "linear"
+            )
+        );
+    }
+
+    public void ToggleUI()
 	{
 		startUI.SetActive(!startUI.activeSelf);
 		restartUI.SetActive(!restartUI.activeSelf);
